43c78eefb767d28695f4ed154c250b84ead1d4bd
Arkad-7-7-7 <nes7history@gmail.com>
2026-02-24 12:06:47 +0100

    Update auth.js

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -8,7 +8,7 @@ exports.handler = async (event) => {
   if (event.httpMethod === "OPTIONS") return { statusCode: 200, headers, body: "" };
   try {
     const { password } = JSON.parse(event.body);
-    if (password === process.env.ADMIN_PASSWORD) {
+    if (password === "Freedom777*") {
       const token = jwt.sign(
         { role: "admin" },
         process.env.JWT_SECRET || "secret",
